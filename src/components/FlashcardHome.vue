3bbc8da7b4cffaadcc6104c65a54ecd0fa47a8fa
Bryce G <17170670+bdc4@users.noreply.github.com>
2023-08-10 12:08:32 -0700

    updates

---
+++ src/components/FlashcardHome.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="greetings">
+      <h3>
+        Flashcards {{ baz }}
+      </h3>
+    </div>
+  </template>
+
+  <script>
+  import $ from 'jquery'
+  export default {
+    data () {
+        return {
+            foo: 'bar'
+        }
+    },
+    computed: {
+        baz () {
+            return this.getFlashcards()
+        }
+    },
+    methods: {
+        getFlashcards () {
+            return $.get({
+                url: 'https://www.github.com/queryluke/masseffect-5e-data/blob/master/docs/v144/en/powers.json'
+            })
+        }
+    }
+  }
+  </script>
+  
